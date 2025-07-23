b6e23846746e74ce23c13f8b09d18a4054235e62
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2025-07-23 12:16:53 +0545

    manifest file with module related information

---
+++ school/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "School",
+    "version": "18.0.1.0",
+    "depends": ["base"],
+    "author": "Lekhnath",
+    "category": "Education",
+    "description": """
+        School Management Module
+    """,
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+}
