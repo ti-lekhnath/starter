6cc30ebf3d142133ea68543408409ed19dc82cce
Lekhnath Parajuli <lekhnathparajuli4@gmail.com>
2025-07-23 16:07:40 +0545

    add some new fields on student table

--- school/models/Student.py
+++ school/models/Student.py
@@ -5,10 +5,14 @@ class Student(models.Model):
     _name = "school.student"
     _description = "Student Information"
 
+    profile = fields.Image(string="Profile Image")
     name = fields.Char(string="Student Name", required=True)
     age = fields.Integer(string="Age", required=True)
+    score = fields.Float(string="Score", required=True)
+    gender = fields.Selection([("Male", "male"), ("Female", "female")])
+    age = fields.Integer(string="Age", store=True)
     roll_number = fields.Char(string="Roll Number")
 
-    course_id = fields.Many2one('school.course', string="Course", required=True)
+    course = fields.Many2one('school.course', string="Course", required=True)
 
 
